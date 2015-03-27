2179bd22878c3c984bc0bebc1b8d12deb4e0037f
fivetools <briley@fivetoolsoftware.com>
2015-03-27 13:49:31 -0400

    Initial check in

---
+++ GitHub_Sandbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHub_Sandbox.Startup))]
+namespace GitHub_Sandbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
